34e44f774cbbfa7bc8d677b8a6098242b29285ed
Moch Luthfi Hadiwidjaja <50488461+luthfihadi78@users.noreply.github.com>
2019-05-17 16:56:34 +0700

    Add files via upload

---
+++ Day 5/latihan1.js
@@ -0,0 +1,39 @@
+1. prompt("Masukan Angka")
+
+
+var score = 20;
+if(score >= 85 && < 100) {
+  console.log("Grade A");
+}
+else if
+  (score >= 75 && <= 84) {
+    console.log("Grade B");
+  }
+ else if(score >= 65 && <= 74) {
+    console.log("Grade C");
+  }
+  else if(score  >= 55 && <=64 ) {
+    console.log("Grade D");
+  } 
+  else (score  >= 0 && <=54 ) {
+    console.log("Grade E");
+  }
+
+2. var penumpang =prompt("Berapa total penumpang")
+
+if (penumpang <= 2){
+  console.log("Kecepatan 20km/jam")
+}
+else {
+  if(penumpang == 3){
+  console.log("Kecepatan 15km/jam")
+}
+else {
+  if(penumpang == 4){
+  console.log("Kecepatan 13km/jam")
+  } 
+else {
+  console.log("sudah penuh")
+}  
+
+  
